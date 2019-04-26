83252531cf44120fa81478e31c2d3a5fa16db644
Gerardo Martinez <gerardo.mr@icloud.com>
2019-04-25 20:36:21 -0700

    Work In Progress - Updated Library

--- AltIMU_v3.py
+++ AltIMU_v3.py
@@ -1,26 +1,24 @@
 from I2C import I2C
-from constants import *
+from LSM303D import LSM303D, AccelScale, MagScale
+from LSM303D import Registers as LSM303DRegisters
+from L3GD20H import L3GD20H, GyroScale
+from L3GD20H import Registers as L3GD20HRegisters
 
 
 class AltIMUv3(I2C):
 
-    # Output registers used by the accelerometer
-    accel_registers = [
-        LSM303D.OUT_X_L_A,
-        LSM303D.OUT_X_H_A,
-        LSM303D.OUT_Y_L_A,
-        LSM303D.OUT_Y_H_A,
-        LSM303D.OUT_Z_L_A,
-        LSM303D.OUT_Z_H_A,
-    ]
-
-    def __init__(self, bus_id=1):
+    def __init__(self,
+                 accel_scale=AccelScale.G_2,
+                 mag_scale=MagScale.Gauss_4,
+                 gyro_scale=GyroScale.DPS_2000,
+                 bus_id=1):
         """ 
         Set up I2C connection and initialize some flags and values.
         """
 
         super(AltIMUv3, self).__init__(bus_id)
-        self.is_accel_enabled = False
+        self.accel_mems = LSM303D(accel_scale, mag_scale)
+        self.gyro_mems = L3GD20H(gyro_scale)
 
     def __del__(self):
         """ 
@@ -28,35 +26,46 @@ class AltIMUv3(I2C):
         """
         try:
             # Power down MEMS
-            self.write_register(LSM303D_Address, LSM303D.CTRL1, 0x00)
+            self.write_register(self.accel_mems.address, LSM303DRegisters.CTRL1, 0x00)
+            self.write_register(self.accel_mems.address, LSM303DRegisters.CTRL7, 0x03)
 
             super(AltIMUv3, self).__del__()
         except:
             pass
 
-    def enable(self, accelerometer=True):
-        if accelerometer:
-            self.write_register(LSM303D_Address, LSM303D.CTRL1, 0xA7)
-            self.write_register(LSM303D_Address, LSM303D.CTRL2, 0x00)
-            self.is_accel_enabled = True
+    def enable(self):
+
+        # Accelerometer ODR selection and enable (800 Hz)
+        self.write_register(self.accel_mems.address, LSM303DRegisters.CTRL1, 0x97)
+
+        # Accelerometer FS selection
+        ctrl_2_value = self.accel_mems.accel_scale << 3
+        self.write_register(self.accel_mems.address, LSM303DRegisters.CTRL2, ctrl_2_value)
+
+        # Magnetometer enable
+        self.write_register(self.accel_mems.address, LSM303DRegisters.CTRL7, 0x00)
+
+        # Magnetometer ODR selection (50Hz)
+        self.write_register(self.accel_mems.address, LSM303DRegisters.CTRL5, 0x70)
+
+        # Magnetometer FS selection
+        ctrl_6_value = self.accel_mems.mag_scale << 5
+        self.write_register(self.accel_mems.address, LSM303DRegisters.CTRL6, ctrl_6_value)
 
     def get_accelerometer_raw(self):
         """
         Return a 3D vector of raw accelerometer data.
         """
 
-        # Check if accelerometer has been enabled
-        if not self.is_accel_enabled:
-            raise(Exception('Accelerometer is not enabled!'))
-
-        return self.read_sensor(LSM303D_Address, self.accel_registers)
+        return self.read_sensor(self.accel_mems.address,
+                                self.accel_mems.accel_axis_output_registers)
 
     def get_accelerometer_cal(self):
         """
         Return a 3D vector of calibrated accelerometer data.
         """
         raw = self.get_accelerometer_raw()
-        scaling = 0.061 / 1000
+        scaling = self.accel_mems.accel_scale.scaling_factor / 1000
 
         cal_x = raw[0] * scaling
         cal_y = raw[1] * scaling
--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from AltIMU_v3 import AltIMUv3
+from filters import LowPassFilter, HighPassFilter
+from time import sleep
 
+# Setup Altimu
 altimu = AltIMUv3()
 altimu.enable()
 
+# Initialize a low pass filter with a default value and a bias of 80%
+low_pass_filter = LowPassFilter([0.0, 0.0, 1.0], 0.8)
+
 while True:
-    accel = altimu.get_accelerometer_raw()
-    print(accel)
+    # Calibrated acceleration
+    accel = altimu.get_accelerometer_cal()
+
+    # Estimated gravity
+    estimated_gravity = low_pass_filter.update_measurement(accel)
+
+    # Estimated linear acceleration
+    linear_x = accel[0] - estimated_gravity[0]
+    linear_y = accel[1] - estimated_gravity[1]
+    linear_z = accel[2] - estimated_gravity[2]
+    linear_acceleration = [linear_x, linear_y, linear_z]
+
+    print(f"Accel\tx: {accel[0]: .6f}\ty: {accel[1]: .6f}\tz: {accel[2]: .6f} [g]")
+    print(f"Gravity\tx: {estimated_gravity[0]: .6f}\ty: {estimated_gravity[1]: .6f}\tz: {estimated_gravity[2]: .6f} [g]")
+    print(f"LinearA\tx: {linear_acceleration[0]: .6f}\ty: {linear_acceleration[1]: .6f}\tz: {linear_acceleration[2]: .6f} [g]")
+
+    sleep(0.1)
+
+
+# # Initialize a high pass filter with a default value and a bias of 80%
+# high_pass_filter = HighPassFilter([0.0, 0.0, 0.0], 0.8)
+#
+# while True:
+#     gyro = altimu.get_gyro_cal()
+#     undrifted_gyro = high_pass_filter.update_measurement(gyro)
+#
+
